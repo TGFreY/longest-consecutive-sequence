e1d142f79b09abf553321521665cef9b30a5e0ed
TGFreY <t.g.frey19@gmail.com>
2018-10-01 18:53:03 +0300

    implement collection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let maxChainLength = 0;
+  let collection = new Set(array);
+
+  for (let number of array) {
+    let currentChain = number + 1;
+    let currentChainLength = 1;
+    while (collection.has(currentChain++)) {
+      currentChainLength++;
+    }
+    maxChainLength = Math.max(maxChainLength, currentChainLength);
+  }
+  return maxChainLength;
 }
